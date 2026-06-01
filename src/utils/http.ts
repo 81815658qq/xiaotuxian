d424a45fd1e36e733e8a321ec2061214da4823c4
q <81815658@qq.com>
2026-06-01 17:03:24 +0800

    feat: 请求函数，对uni.request 的封装

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -24,3 +24,55 @@ const httpInterceptor = {
 
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
+
+/**
+ * 请求函数
+ * @param  UniApp.RequestOptions
+ * @returns Promise
+ *  1. 返回 Promise 对象，用于处理返回值类型
+ *  2. 获取数据成功
+ *    2.1 提取核心数据 res.data
+ *    2.2 添加类型，支持泛型
+ *  3. 获取数据失败
+ *    3.1 401错误  -> 清理用户信息，跳转到登录页
+ *    3.2 其他错误 -> 根据后端错误信息轻提示
+ *    3.3 网络错误 -> 提示用户换网络
+ */
+interface data<T> {
+  code: string
+  msg: string
+  result: T
+}
+
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data as data<T>)
+        } else if (res.statusCode === 401) {
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({
+            url: '/pages/login/login',
+          })
+          reject(res)
+        } else {
+          uni.showToast({
+            title: (res.data as data<T>).msg || '请求失败',
+            icon: 'none',
+          })
+          reject(res)
+        }
+      },
+      fail(err) {
+        uni.showToast({
+          title: '网络异常，请稍后再试',
+          icon: 'none',
+        })
+        reject(err)
+      },
+    })
+  })
+}
